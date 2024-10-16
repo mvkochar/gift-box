37341af60128b017d1710871ebb476f1cf2d3ddc
sgkochar <sgkochar@gmail.com>
2024-10-16 13:52:45 +0300

    add Featured, Follow, Partners, Shipping blocks to Home page, create Footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import './css/Footer.css'
 
 const Footer = () => {
   return (
-    <footer>
-       Footer
+    <footer className='footer'>
+      <div className="footer-bar d-f jc-sb">
+        <ul className="footer-bl-list">
+          <li><a href="">Home</a></li>
+          <li><a href="">Catalogue</a></li>
+          <li><a href="">Services</a></li>
+          <li><a href="">Payment</a></li>
+          <li><a href="">Delivery</a></li>
+          <li><a href="">Return</a></li>
+          <li><a href="">Privacy policy</a></li>
+          <li><a href="">Terms & Conditions</a></li>
+        </ul>
+        <ul className="footer-bl-list">
+          <li><a href="">Birthday Gift</a></li>
+          <li><a href="">Christmas</a></li>
+          <li><a href="">New Year</a></li>
+          <li><a href="">Wedding</a></li>
+          <li><a href="">Valentine’s Day</a></li>
+          <li><a href="">Baby Shower</a></li>
+          <li><a href="">Mother’s Day</a></li>
+          <li><a href="">Father’s Day</a></li>
+        </ul>
+        <div>
+          <h3 className="footer-bl-title">Information</h3>
+          <p className="footer-bl-desc">Simple Brown Box Ltd</p>
+          <p className="footer-bl-desc" style={{ marginTop: "20px" }}>
+            Flat 44 <br />
+            Manley Heights <br />
+            1 South Way <br />
+            Wembley <br />
+            HA9 0JU
+          </p>
+          <p className="footer-bl-desc" style={{ marginTop: "19px" }}>Phone: +44 1234567899</p>
+          <p className="footer-bl-desc" style={{ marginTop: "11px" }}>
+            Company@SimpleBrownBox.co.uk <br />
+            Service@SimpleBrownBox.co.uk <br />
+            Hello@SimpleBrownBox.co.uk
+          </p>
+        </div>
+        <div>
+          <h3 className="footer-bl-title">My account</h3>
+          <ul className="footer-bl-list">
+            <li><a href="">Registration</a></li>
+            <li><a href="">Log in</a></li>
+            <li><a href="">My wishlist</a></li>
+            <li><a href="">View bag</a></li>
+          </ul>
+          <div className="footer-payment">
+            <div><img src="/images/footer-payment.png" alt="footer-payment" /></div>
+            <p className="footer-payment-note">
+              <span>We accept all major cards.</span> All the payments
+              are made through the Secure Platform <span>Paypal</span>.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyrights">Copyright &copy; 2022 «Simple Brown Box»</div>
     </footer>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,6 +16,61 @@ const Home = () => {
         because we want each recipient to feel as special as they truly are.
       </div>
       <div className="home-divider"></div>
+      <section className='home-featured'>
+        <h2 className="home-bl-title">Featured collections</h2>
+        <div className="home-featured-box d-f">
+          <div>
+            <div><img src="/images/home-featured1.png" alt="home-featured1" /></div>
+            <a href="" className="home-featured-link">For her</a>
+          </div>
+          <div>
+            <div><img src="/images/home-featured2.png" alt="home-featured2" /></div>
+            <a href="" className="home-featured-link">Wedding</a>
+          </div>
+          <div>
+            <div><img src="/images/home-featured3.png" alt="home-featured3" /></div>
+            <a href="" className="home-featured-link">Littles</a>
+          </div>
+        </div>
+      </section>
+      <section className='home-bottom'>
+        <div className="home-follow">
+          <h2 className="home-bl-title">Follow our feed</h2>
+          <div className="home-follow-box d-f align-center">
+            <div>
+              <div className="home-follow-insta">@simple_Brown_box</div>
+              <a href="" className="home-follow-link">Follow us</a>
+            </div>
+            <div className="home-folow-images d-f">
+              <div><img src="/images/insta1.png" alt="insta1" /></div>
+              <div className="little-images d-f">
+                <div><img src="/images/insta2.png" alt="insta2" /></div>
+                <div><img src="/images/insta3.png" alt="insta3" /></div>
+                <div><img src="/images/insta4.png" alt="insta4" /></div>
+                <div><img src="/images/insta5.png" alt="insta5" /></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="home-partners">
+          <h2 className="home-bl-title">Partners</h2>
+          <div className="home-partners-box d-f">
+            <div><img src="/images/partner1.png" alt="partner1" /></div>
+            <div><img src="/images/partner2.png" alt="partner2" /></div>
+            <div><img src="/images/partner3.png" alt="partner3" /></div>
+            <div><img src="/images/partner4.png" alt="partner4" /></div>
+            <div><img src="/images/partner5.png" alt="partner5" /></div>
+          </div>
+        </div>
+        <div className="home-divider"></div>
+        <div className="home-shipping d-f">
+          <div className="home-shipping-info d-f align-center">
+            <h2 className="home-shipping-title">Free Shipping all over great britain via royal mail special delivery</h2>
+            <div><img src="/images/shipping-logo.png" alt="shipping-logo" /></div>
+          </div>
+          <button className='page-top-btn' onClick={()=>window.scrollTo(0, 0)}><img src="/images/arrow-up.png" alt="arrow-up" /></button>
+        </div>
+      </section>
     </>
   )
 }
